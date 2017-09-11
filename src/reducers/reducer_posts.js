4d0eed92740ea25ca43c72ea4d1900e6b07553ec
cbrwflo <cbrwflo@users.noreply.github.com>
2017-09-10 22:40:03 -0700

    Add new post and delete post functions

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,24 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions'; //same as ../actions/index
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions'; //same as ../actions/index
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST: 
+      //drop key:value pairs where key matches 2nd param
+      return _.omit(state, action.payload);
+
+    case FETCH_POST: 
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;
+      // return newState;
+
+      // ES6: Key interpolation. Same as above.
+      return { ...state, [action.payload.data.id]: action.payload.data};
+
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
-
+    
     default:
       return state;
   }
